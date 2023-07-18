e55c9cd7e6a325095eb4c512d73b7c1f3b9571d6
gary wallace <garyatgreatvacation4u@gmail.com>
2023-07-18 19:23:40 -0400

    added tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow:{
+        "big-white":"1px 1px 20px 1px white",
+        "big-1":"1px 1px 20px 2px #172A3A",
+        "big-2":"1px 1px 20px 2px #004346",
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        "big-3":"1px 1px 20px 2px #004346,-1px -1px 20px 2px #004346"
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        xs:'275px',
+        tablet:"640px",
+        laptop:"1024px",
+        desktop:"1280px"
+        
+      },
     },
   },
   plugins: [],
