2cb5df23b1724d59effe6e603a92e04ed88703b2
gary wallace <garyatgreatvacation4u@gmail.com>
2023-07-18 19:17:35 -0400

    added images in nextCongig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,52 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        unoptimized:true,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'new-master.s3.ca-central-1.amazonaws.com',
+            port: '',
+            // pathname: '/account123/**',
+          },
+          {
+            protocol: 'http',
+            hostname: 'localhost',
+            port: '3000',
+            // pathname: '/account123/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'masterultils.com',
+            port: '',
+            // pathname: '/account123/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'picsum.photos/200',
+            port: '',
+            // pathname: '/account123/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'cdn.weatherapi.com',
+            port: '',
+            // pathname: '/account123/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'webtoons-static.pstatic.net',
+            port: '',
+            // pathname: '/account123/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'media.rawg.io',
+            port: '',
+            // pathname: '/account123/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
