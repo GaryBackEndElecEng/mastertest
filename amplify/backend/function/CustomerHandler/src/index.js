aa255a1fe230db6e57839614d647f4e9e9ded337
gary wallace <garyatgreatvacation4u@gmail.com>
2023-07-19 14:47:53 -0400

    installed aws amplify @aws-amplify/ui-react

--- amplify/backend/function/CustomerHandler/src/index.js
+++ amplify/backend/function/CustomerHandler/src/index.js
@@ -6,7 +6,7 @@
 exports.handler = async (event) => {
     console.log(event);
     const customerId = event.pathParameters.customerId; ///customers/id
-    const customer={"customerId":customerId,"customerName":"Customer" + custumerId};
+    const customer={"customerId":customerId,"customerName":"Customer" + customerId};
     const response={
         statusCode:200,
     //  Uncomment below to enable CORS requests
