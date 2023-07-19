3e4ce526afb605a2bb4a216c4c13f823aa4bf75e
gary wallace <garyatgreatvacation4u@gmail.com>
2023-07-19 13:34:29 -0400

    env.production/development and aws hello world hook

---
+++ amplify/backend/function/CustomerHandler/src/index.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(event);
+    const customerId = event.pathParameters.customerId; ///customers/id
+    const customer={"customerId":customerId,"customerName":"Customer" + custumerId};
+    const response={
+        statusCode:200,
+    //  Uncomment below to enable CORS requests
+     headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Headers": "*"
+     },
+        body: JSON.stringify(customer),
+    };
+    return response;
+};
